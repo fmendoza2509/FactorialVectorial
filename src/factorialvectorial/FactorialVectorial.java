bfec257c7e6ca0a9b0641885a90d1cea002ceac4
frangusdj <frangusdj@hotmail.com>
2020-05-01 18:39:58 -0500

    hasta aqui funciona el factorial de n numeros de entrada pero tipo string

--- src/factorialvectorial/FactorialVectorial.java
+++ src/factorialvectorial/FactorialVectorial.java
@@ -1,11 +1,24 @@
 package factorialvectorial;
 
-import java.util.Scanner;
-
 public class FactorialVectorial {
+    public static void Factorial(int n){
+        int fact =1;
+        if((n==0)||(n==1)){
+            fact=1;
+        }else if(n==2){
+            fact = 2;
+        }else if(n>2){
+            for(int i=2;i<=n;i++){
+                fact *=i;
+            }
+        }
+        System.out.println(fact);
+    }
     public static void factorialArray(String a[]){
+        int n =0;
         for(int i=0;i<a.length;i++){
-            System.out.println(a[i]);
+            n = Integer.parseInt(a[i]);
+            Factorial(n);
         } 
     }
     public static void main(String[] args) {
